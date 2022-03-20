cf47eca1630cb71836352a39d170d16aba44232b
YSH <devtlin34@gmail.com>
2022-03-19 17:50:02 -0700

    Add download/update urls

--- youtube_long.user.js
+++ youtube_long.user.js
@@ -2,11 +2,12 @@
 // @name         Youtube Long
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @author       zero41120
 // @description  Observe url and redirect from youtube.com/short to youtube.com/watch?v=
 // @match        https://www.youtube.com/*
 // @icon         data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDk4Ljk0IDEyMi44OCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgOTguOTQgMTIyLjg4IiB4bWw6c3BhY2U9InByZXNlcnZlIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsLXJ1bGU6ZXZlbm9kZDtjbGlwLXJ1bGU6ZXZlbm9kZDtmaWxsOiNGNDA0MDc7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik02My40OSwyLjcxYzExLjU5LTYuMDQsMjUuOTQtMS42NCwzMi4wNCw5LjgzYzYuMSwxMS40NywxLjY1LDI1LjY2LTkuOTQsMzEuN2wtOS41Myw1LjAxIGM4LjIxLDAuMywxNi4wNCw0LjgxLDIwLjE0LDEyLjUyYzYuMSwxMS40NywxLjY2LDI1LjY2LTkuOTQsMzEuN2wtNTAuODIsMjYuN2MtMTEuNTksNi4wNC0yNS45NCwxLjY0LTMyLjA0LTkuODMgYy02LjEtMTEuNDctMS42NS0yNS42Niw5Ljk0LTMxLjdsOS41My01LjAxYy04LjIxLTAuMy0xNi4wNC00LjgxLTIwLjE0LTEyLjUyYy02LjEtMTEuNDctMS42NS0yNS42Niw5Ljk0LTMxLjdMNjMuNDksMi43MSBMNjMuNDksMi43MXogTTM2LjA2LDQyLjUzbDMwLjc2LDE4Ljk5bC0zMC43NiwxOC45VjQyLjUzTDM2LjA2LDQyLjUzeiIvPjwvZz48L3N2Zz4=
 // @grant        none
+// @updateURL    https://github.com/zero41120/YoutubeLongs/raw/main/youtube_long.user.js
+// @downloadURL  https://github.com/zero41120/YoutubeLongs/raw/main/youtube_long.user.js
 // ==/UserScript==
 
 const shortsUrl = 'https://www.youtube.com/shorts/';
